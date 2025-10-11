665a46d932971e9f93c85f27ef723ea7757ab906
Jeongtae Shin <shinjeongtae@gmail.com>
2025-10-11 23:51:49 +0900

    [ADD] add user function getpinfo(for testing)

--- user/getpinfo.c
+++ user/getpinfo.c
@@ -1,18 +1,34 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "user/user.h"
+#include "kernel/pstat.h"
+#include "user.h"
 
-void
-main(void) {
-  struct pstat s;
-  if (getpinfo(&s) < 0) {
-    printf("Error retrieving process information.\n");
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    printf("Usage: getpinfo <pid>\n");
     exit(1);
   }
-  printf("Process Information:\n");
+
+  int pid = atoi(argv[1]);
+  struct pstat st;
+
+  if (getpinfo(&st) < 0) {
+    printf("getpinfo failed\n");
+    exit(1);
+  }
+
   for (int i = 0; i < NPROC; i++) {
-    if (s.inuse[i]) {
-      printf("PID: %d, Tickets: %d, Executed: %d\n", s.pid[i], s.lottery_tickets[i], s.times_executed[i]);
+    if (st.inuse[i] && st.pid[i] == pid) {
+      printf("Process Info for PID %d:\n", pid);
+      printf("Tickets: %d\n", st.lottery_tickets[i]);
+      printf("pid: %d\n", st.pid[i]);
+      printf("Executed: %d\n", st.times_executed[i]);
+      exit(0);
     }
   }
+
+  printf("Process with PID %d not found.\n", pid);
+  exit(1);
 }
